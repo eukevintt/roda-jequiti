2e6d2c2bc2a1fc17bac18a183cc7b053dde38c5b
Kevin Araujo <kevinaraujoo677@gmail.com>
2021-09-14 16:09:09 -0300

    adicionandos os arquivos

---
+++ basededados.py
@@ -0,0 +1,36 @@
+base_de_dados = {
+    'Quarto': [
+        'Camá',
+        'Televisão',
+        'Quadro'
+    ],
+    'Cozinha': [
+        'Cadeira',
+        'Mesa',
+        'Comida',
+        'Computador'
+    ],
+    'Frutas': [
+        'Abacate',
+        'Abacaxi',
+        'Acerola',
+        'Banana',
+        'Cacau',
+        'Cajá',
+        'Cajú'
+    ],
+    'Animal': [
+        'Abelha',
+        'Andorinha',
+        'Anta',
+        'Arara',
+        'Aranha',
+        'Cabra',
+        'Cachorro',
+        'Gato',
+        'Calango',
+        'Cardeal',
+        'Carrapato',
+        'Emu'
+    ]
+}
